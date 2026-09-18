81bf2af10f24431e4562b5ca3774f09a4ed906b5
gabriell <gabriell.dunder@gmail.com>
2026-09-18 11:09:28 -0300

    salvar

---
+++ AULA02/conceito_tipos_de_dados.js
@@ -0,0 +1,33 @@
+// Tipos de dados 
+// No javascript, existem diferentes tipos de dados que podem ser usados para armazenar informações. Alguns dos tipos de dados mais comuns incluem:
+
+// 1. Number: Representa valores numéricos, tanto inteiros quanto decimais.
+///let idade = 30; // número inteiro
+///let altura = 1.75; // número decimal
+// string (texto) 
+// Number (Numeros)
+// Boolean (verdadeiro ou falso)
+// Object (objeto) 
+// Array (lista de valores)
+// Nul (Valor Vazio)
+// Undefined (Valor não definido)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+// STRING (Texto)
+// Uma string é uma sequência de caracteres usada para representar texto. Strings são delimitadas por aspas simples (' ') ou aspas duplas (" ").
+let nome = "JARVIS";
+let mensagem = 'Olá, mundo!';
+
+console.log(nome);
+console.log(mensagem);
+ 
+let saudacao = "Olá, " + nome + "!";
+console.log(saudacao); // Saída: Olá, JARVIS! 
+
+// typeof
+// o typeof serve para descobrir o tipo de dado de uma variável ou valor. Ele retorna uma string que indica o tipo do operando.
+let nomeDois = "JARVIS";
+let nomeTres = "gabriell";
+console.log(typeof nomeDois); // Saída: string
+
+let soma = 10 + 5;
+console.log(typeof soma); // Saída: number
---
+++ AULA02/exercicio_01.js
@@ -0,0 +1,8 @@
+// atividade number
+// crie uma variável chama anoAtual e outra e outra chamada anoNascimento
+// calcule e exiba a idade no(a) consele
+
+let anoAtual = 2024;
+let anoNascimento = 1990;
+let idade = anoAtual - anoNascimento;
+console.log("A idade é: " + idade);
